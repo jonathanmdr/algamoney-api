92d0108a9d5e3ac639992ca7a95bf751ef573c87
Jonathan Henrique Medeiros <jonathan.mdr@hotmail.com>
2019-02-17 21:38:34 -0300

    Nome do usuário no token JWT.

---
+++ src/main/java/com/algaworks/algamoney/api/security/UsuarioSistema.java
@@ -0,0 +1,22 @@
+package com.algaworks.algamoney.api.security;
+
+import com.algaworks.algamoney.api.model.Usuario;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class UsuarioSistema extends User {
+
+    private static final long serialVersionUID = 1L;
+
+    @Getter
+    private Usuario usuario;
+
+    public UsuarioSistema(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+        super(usuario.getEmail(), usuario.getSenha(), authorities);
+        this.usuario = usuario;
+    }
+
+}
