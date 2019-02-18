9e317a1faeef94aebab196538fd3d8e60d11aec9
Jonathan Henrique Medeiros <jonathan.mdr@hotmail.com>
2019-02-17 22:25:12 -0300

    Implementando atualização de lançamentos.

--- src/main/java/com/algaworks/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/LancamentoResource.java
@@ -21,15 +21,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -83,6 +75,17 @@ public class LancamentoResource {
     @PreAuthorize("hasAuthority('ROLE_REMOVER_LANCAMENTO') and #oauth2.hasScope('write')")
     public void Remover(@PathVariable Long id) {
         lancamentoRepository.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO') and #oauth2.hasScope('write')")
+    public ResponseEntity<Lancamento> atualizar(@PathVariable Long id, @Valid @RequestBody Lancamento lancamento) {
+        try {
+            Lancamento lancamentoSalvo = lancamentoService.atualizar(id, lancamento);
+            return ResponseEntity.ok(lancamentoSalvo);
+        } catch(IllegalArgumentException ex) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @ExceptionHandler(PessoaInexistenteOuInativaException.class)
--- src/main/java/com/algaworks/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/algaworks/algamoney/api/service/LancamentoService.java
@@ -5,6 +5,7 @@ import com.algaworks.algamoney.api.model.Pessoa;
 import com.algaworks.algamoney.api.repository.LancamentoRepository;
 import com.algaworks.algamoney.api.repository.PessoaRepository;
 import com.algaworks.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,5 +26,39 @@ public class LancamentoService {
         }
 
         return lancamentoRepository.save(lancamento);
+    }
+
+    public Lancamento atualizar(Long id, Lancamento lancamento) {
+        Lancamento lancamento1Salvo = buscarLancamentoExistente(id);
+
+        if (!lancamento.getPessoa().equals(lancamento1Salvo.getPessoa())) {
+            validarPessoa(lancamento);
+        }
+
+        BeanUtils.copyProperties(lancamento, lancamento1Salvo, "id");
+
+        return lancamentoRepository.save(lancamento1Salvo);
+    }
+
+    private void validarPessoa(Lancamento lancamento) {
+        Pessoa pessoa = null;
+
+        if (lancamento.getPessoa().getId() != null) {
+            pessoa = pessoaRepository.findOne(lancamento.getPessoa().getId());
+        }
+
+        if (pessoa == null || pessoa.isInativo()) {
+            throw new PessoaInexistenteOuInativaException();
+        }
+    }
+
+    private Lancamento buscarLancamentoExistente(Long id) {
+        Lancamento lancamentoSalvo = lancamentoRepository.findOne(id);
+
+        if (lancamentoSalvo == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return lancamentoSalvo;
     }
 }
