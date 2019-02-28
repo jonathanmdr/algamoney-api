ae4cfe88549d852cf35448547f74aa28070e8719
Jonathan Henrique Medeiros <jonathan.mdr@hotmail.com>
2019-02-27 22:13:42 -0300

    Bug fixes no CorsFilter

--- src/main/java/com/algaworks/algamoney/api/cors/CorsFilter.java
+++ src/main/java/com/algaworks/algamoney/api/cors/CorsFilter.java
@@ -35,7 +35,7 @@ public class CorsFilter implements Filter {
 
         if ("OPTIONS".equals(request.getMethod()) && algaMoneyApiProperty.getOriginPermitida().equals(request.getHeader("Origin"))) {
             response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
-            response.setHeader("Access-Control-Allow-Methods", "Authorization, Content-Type, Accept");
+            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
             response.setHeader("Access-Control-Max-Age", "3600");
 
             response.setStatus(HttpServletResponse.SC_OK);
