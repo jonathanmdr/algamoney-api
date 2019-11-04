ead5d249f4e3a5fbafd128ed741e1a6bd73efff6
Jonathan Henrique Medeiros <jonathan.mdr@hotmail.com>
2019-11-04 00:13:05 -0300

    Criando o método para salvar um arquivo temporariamente no S3.

---
+++ src/main/java/com/algaworks/algamoney/api/storage/S3.java
@@ -0,0 +1,63 @@
+package com.algaworks.algamoney.api.storage;
+
+import com.algaworks.algamoney.api.config.property.AlgaMoneyApiProperty;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.UUID;
+
+@Component
+public class S3 {
+
+    private static final Logger logger = LoggerFactory.getLogger(S3.class);
+
+    @Autowired
+    private AmazonS3 amazonS3;
+
+    @Autowired
+    private AlgaMoneyApiProperty property;
+
+    public String salvarTemporariamente(MultipartFile arquivo) {
+        AccessControlList accessControlList = new AccessControlList();
+        accessControlList.grantPermission(GroupGrantee.AllUsers, Permission.Read);
+
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(arquivo.getContentType());
+        objectMetadata.setContentLength(arquivo.getSize());
+
+        String nomeUnicoArquivo = gerarNomeunicoArquivo(arquivo.getOriginalFilename());
+
+        try {
+            PutObjectRequest putObjectRequest = new PutObjectRequest(
+                    property.getS3().getBucket(),
+                    nomeUnicoArquivo,
+                    arquivo.getInputStream(),
+                    objectMetadata).withAccessControlList(accessControlList);
+
+            putObjectRequest.setTagging(new ObjectTagging(
+                    Arrays.asList(new Tag("expirar", "true"))
+            ));
+
+            if (logger.isDebugEnabled()) {
+                logger.debug("Arquivo {} enviado com sucesso para o S3.", arquivo.getOriginalFilename());
+            }
+
+            return nomeUnicoArquivo;
+        } catch (IOException ex) {
+            throw  new RuntimeException("Não foi possível enviar o arquivo para o AWS Bucket S3.", ex);
+        }
+
+    }
+
+    private String gerarNomeunicoArquivo(String originalFilename) {
+        return UUID.randomUUID().toString() + "_" + originalFilename;
+    }
+
+}
